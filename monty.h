9962b05d127a6db7c7e3e727acd115ca6f65435d
Apiyo120 <apiyodianah@gmail.com>
2023-06-22 11:56:43 -0700

    Monty

--- monty.h
+++ monty.h
@@ -41,5 +41,8 @@ void pall(stack_t **stack, unsigned int line_number);
 void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 int is_numeric(char *str);
+void pop(stack_t **stack, unsigned int line_number)
+void add(stack_t **stack, unsigned int line_number)
+void swap(stack_t **stack, unsigned int line_number)
 
 #endif 
